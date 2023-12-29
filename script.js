d74c3d34388a5bd3cb3c948d78faa76a7346ec01
nicopressez <154272808+nicopressez@users.noreply.github.com>
2023-12-29 15:58:03 +0100

    Refactored code

--- script.js
+++ script.js
@@ -12,19 +12,6 @@ const Player = (sign,starting,name) =>
 
  return {getSign, changeTurn,turn,name}}
 
-function initiatePlayerInfo(){
-const player1Name = document.getElementById("player1Name");
-const player1Sign = document.getElementById("player1Sign");
-player1Name.textContent = Player1.name;
-player1Sign.textContent = Player1.getSign();
-
-const player2Name = document.getElementById("player2Name");
-const player2Sign = document.getElementById("player2Sign");
-player2Name.textContent = Player2.name;
-player2Sign.textContent = Player2.getSign();
-}
-
-
 const gameBoard = (() => {
  const gameGrid = ["","","","","","","","",""];
 
@@ -39,23 +26,23 @@ const gameBoard = (() => {
 })();
 
 
-function playRound(event,index, player){
- if(event.target.textContent !== ""){return;}
+const roundProcess = (() => {
 
- gameBoard.setGridSign(index, player.getSign());
- event.target.textContent = gameBoard.gameGrid[index];
- checkWinner();
- checkFullBoard();
+    function playRound(event,index, player){
+    if(event.target.textContent !== ""){return;}
 
- if (player = Player1){Player2.changeTurn()}
- else if (player = Player2){Player1.changeTurn()}
- player.changeTurn();;
-}
+    gameBoard.setGridSign(index, player.getSign());
+     event.target.textContent = gameBoard.gameGrid[index];
+    checkWinner();
+    checkFullBoard();
 
- // If someone's winning or grid is full, the game stops and winner is announced
+    if (player = Player1){Player2.changeTurn()}
+     else if (player = Player2){Player1.changeTurn()}
+     player.changeTurn();;
+    }
 
-function checkWinner ()
-{
+    function checkWinner ()
+    {
     const winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     for (const condition of winConditions) {
        const [a, b, c]  = condition;
@@ -65,20 +52,15 @@ function checkWinner ()
         {document.getElementById("gameResult").textContent = `${gameBoard.gameGrid[a]} won!`;
          gameStatus.stopGame()}
 
+    }}
+    function checkFullBoard() {
+        if (gameBoard.isGridFull()) {
+        gameStatus.stopGame();
+            if (document.getElementById("gameResult").textContent == "")
+            {document.getElementById("gameResult").textContent = "It's a tie!"}
+        }
     }
-}
-
-function checkFullBoard() {
-    if (gameBoard.isGridFull()) {
-    gameStatus.stopGame();
-        if (document.getElementById("gameResult").textContent == "")
-        {document.getElementById("gameResult").textContent = "It's a tie!"}
-    }
-}
-
-function findTurn()
-{if (Player1.turn) {return Player1}
-else {return Player2};}
+    return {playRound}})();
 
 const gameStatus = (() => {
 
@@ -96,34 +78,50 @@ const gameStatus = (() => {
 
     const allGrids = [gridZero, gridOne,gridTwo,gridThree,gridFour,gridFive,gridSix,gridSeven,gridEight]
 
-function startGame (){
-    gameActive = true;
-    allGrids.forEach(grid => { grid.textContent = "";});
-    gameBoard.gameGrid.forEach(function(value, index, array) {array[index] = "";})
+    function startGame (){
+     gameActive = true;
+     allGrids.forEach(grid => { grid.textContent = "";});
+     gameBoard.gameGrid.forEach(function(value, index, array) {array[index] = "";})
 
-    gridZero.addEventListener('click',(event) => {if(gameActive) {playRound(event,0, findTurn())}});
-    gridOne.addEventListener('click', (event) => {if(gameActive) {playRound(event,1, findTurn())}});
-    gridTwo.addEventListener('click', (event) => {if(gameActive) {playRound(event,2, findTurn())}});
-    gridThree.addEventListener('click', (event) => {if(gameActive) {playRound(event,3, findTurn())}});
-    gridFour.addEventListener('click', (event) => {if(gameActive) {playRound(event,4, findTurn())}});
-    gridFive.addEventListener('click', (event) => {if(gameActive) {playRound(event,5, findTurn())}});
-    gridSix.addEventListener('click', (event) => {if(gameActive) {playRound(event,6, findTurn())}});
-    gridSeven.addEventListener('click', (event) => {if(gameActive) {playRound(event,7, findTurn())}});
-    gridEight.addEventListener('click', (event) => {if(gameActive) {playRound(event,8, findTurn())}});}
+     gridZero.addEventListener('click',(event) => {if(gameActive) {roundProcess.playRound(event,0, findTurn())}});
+     gridOne.addEventListener('click', (event) => {if(gameActive) {roundProcess.playRound(event,1, findTurn())}});
+     gridTwo.addEventListener('click', (event) => {if(gameActive) {roundProcess.playRound(event,2, findTurn())}});
+     gridThree.addEventListener('click', (event) => {if(gameActive) {roundProcess.playRound(event,3, findTurn())}});
+     gridFour.addEventListener('click', (event) => {if(gameActive) {roundProcess.playRound(event,4, findTurn())}});
+     gridFive.addEventListener('click', (event) => {if(gameActive) {roundProcess.playRound(event,5, findTurn())}});
+     gridSix.addEventListener('click', (event) => {if(gameActive) {roundProcess.playRound(event,6, findTurn())}});
+     gridSeven.addEventListener('click', (event) => {if(gameActive) {roundProcess.playRound(event,7, findTurn())}});
+     gridEight.addEventListener('click', (event) => {if(gameActive) {roundProcess.playRound(event,8, findTurn())}});}
 
-function stopGame(){
- gameActive = false;
-document.getElementById('endGameDialog').showModal();
+    function stopGame(){
+     gameActive = false;
+     document.getElementById('endGameDialog').showModal();
 }
-  return {stopGame, startGame}
-})()
 
-function initiateGame(form){
+    function initiateGame(form){
      Player1 = Player("X", true, `${form.player1NewName.value}`);
      Player2 = Player("O", false, `${form.player2NewName.value}`);
-    
+   
      document.getElementById('definePlayers').close();
-
-     gameStatus.startGame();
+     startGame();
      initiatePlayerInfo();
 }
+
+    function initiatePlayerInfo(){
+     const player1Name = document.getElementById("player1Name");
+     const player1Sign = document.getElementById("player1Sign");
+     player1Name.textContent = Player1.name;
+     player1Sign.textContent = Player1.getSign();
+    
+     const player2Name = document.getElementById("player2Name");
+     const player2Sign = document.getElementById("player2Sign");
+     player2Name.textContent = Player2.name;
+     player2Sign.textContent = Player2.getSign();
+    }
+
+  return {stopGame, startGame, initiateGame}
+})()
+
+function findTurn()
+{if (Player1.turn) {return Player1}
+else {return Player2};}
